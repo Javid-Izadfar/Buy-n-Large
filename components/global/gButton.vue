6accbc3c80d9dcb4b3862f80671a5c42b5ad32c0
Javid Izadfar <javid.izadfar@gmail.com>
2021-09-22 14:57:06 +0330

    feat: added button component

---
+++ components/global/gButton.vue
@@ -0,0 +1,71 @@
+<template>
+    <component :is="to ? 'NuxtLink' : 'button'"
+               :to="to"
+               :event="disabled ? '' : 'click'"
+               :disabled="disabled"
+               :class="[
+                   'button',
+                   `button--${variant}`,
+                   {
+                       'button--block': isBlock,
+                       'button--disabled': disabled
+                   }
+               ]"
+               @click="$emit('click')">
+        <slot/>
+    </component>
+</template>
+
+<script>
+export default {
+    props: {
+        to: {
+            type: Object,
+            default: () => (null),
+        },
+        variant: {
+            type: String,
+            default: 'primary',
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false,
+        },
+        isLoading: {
+            type: Boolean,
+            default: false,
+        },
+        isBlock: {
+            type: Boolean,
+            default: false,
+        },
+    },
+    computed: {
+        disabled() {
+            return this.isLoading || this.isDisabled;
+        },
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+    .button {
+        @include create-button()
+        &--block {
+            padding-right: 0;
+            padding-left: 0;
+            text-align: center;
+            width: 100%;
+            display: block;
+        }
+        &--primary {
+            @include create-button-var($primary, $white)
+        }
+        &--secondary {
+            @include create-button-var($secondary, $white)
+        }
+        &--accent {
+            @include create-button-var($accent, $white)
+        }
+    }
+</style>
