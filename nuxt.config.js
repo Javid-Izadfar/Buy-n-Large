d04fefdc79c1399aea802f7738ba223004ad8b91
Javid Izadfar <javid.izadfar@gmail.com>
2021-09-22 08:17:20 +0330

    chore: first commit

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+
+import path from 'path';
+
+export default {
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'javid-challenge',
+        htmlAttrs: {
+            lang: 'en',
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [
+            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        ],
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [
+    ],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [
+    ],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+    // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+    // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {},
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        extend(config, context) {
+            if (context.isDev) {
+                // Run ESLint on save
+                config.module.rules.push({
+                    enforce: 'pre',
+                    test: /\.(js|vue)$/,
+                    loader: 'eslint-loader',
+                    exclude: /(node_modules)/,
+                });
+            }
+            // Extend only webpack config for client-bundle
+            const src = config.resolve.alias['~'];
+            config.resolve.alias['~root'] = path.join(src, '');
+            config.resolve.alias['~icons'] = path.join(
+                src,
+                'components/icons'
+            );
+            config.resolve.alias['~endpoints'] = path.join(
+                src,
+                'scripts/utils/endpoints.js'
+            );
+        },
+    },
+};
---
+++ scripts/utils/endpoints.js
@@ -0,0 +1,2 @@
+export const PRODUCTS_LIST = '/search/';
+export const PRODUCT_DETAIL = (productId) => `/product/${productId}/`;
